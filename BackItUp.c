b1b3db6e062b9f6e963d80b021bcbb84854d3985
Matias M <matias.moseley@gmail.com>
2023-04-10 12:34:21 -0700

    Assignment Behavior 3 implemented

--- BackItUp.c
+++ BackItUp.c
@@ -9,31 +9,46 @@
 
 #define SIZE 1024
 
+typedef struct node {
+    char *file;
+    struct stat orig, back;
+} info;
+
 void log_result(char *op, char *item) {
     printf("%s | %s | %s\n", op, item, strerror(errno));
     errno = 0;
 }
 
-void copyFile(char *file) {
-    int iFd = open(file, O_RDONLY);
-    log_result("open", file);
-    if (iFd == -1) return;
+void copyFile(void *arg) {
+    info *data = (info *) arg;
+
+    int iFd = open(data->file, O_RDONLY);
+    log_result("open", data->file);
+    if (iFd == -1) { free(data); return; }
 
     char path[SIZE];
-    sprintf(path, ".backup/%s.bak", file);
+    sprintf(path, ".backup/%s.bak", data->file);
+
+    int n = stat(path, &data->back);
+    log_result("stat", path);
+
+    if (n == -1) printf("Creating Backup of %s\n", data->file);
+    else if (data->orig.st_mtime > data->back.st_mtime) printf("WARNING: Overwriting %s\n", data->file);
+    else printf("%s is up to date\n", data->file);
 
     int oFd = open(path, O_WRONLY | O_CREAT, 0644);
     log_result("open", path);
-    if (oFd == -1) { close(iFd); return; }
+    if (oFd == -1) { close(iFd); free(data); return; }
 
     int rd;
     char buffer[SIZE];
     while ((rd = read(iFd, buffer, SIZE)) > 0) write(oFd, buffer, rd);
 
     close(iFd);
-    log_result("close", file);
+    log_result("close", data->file);
     close(oFd);
     log_result("close", path);
+    free(data);
 }
 
 void enterDir(char *directory) {
@@ -49,16 +64,19 @@ void enterDir(char *directory) {
     log_result("mkdir", ".backup");
 
     struct dirent *file;
-    struct stat status;
     while ((file = readdir(dir)) != NULL){
         if (file->d_name[0] == '.') continue;
 
-        int n = stat(file->d_name, &status);
-        log_result("stat", file->d_name);
-        if (n == -1) continue;
+        info *data = (info *) malloc(sizeof(info));
+        data->file = file->d_name;
 
-        if (S_ISDIR(status.st_mode)) enterDir(file->d_name);
-        else if (S_ISREG(status.st_mode)) copyFile(file->d_name);
+        int n = stat(file->d_name, &data->orig);
+        log_result("stat", file->d_name);
+        if (n == -1) { free(data); continue; }
+
+        if (S_ISDIR(data->orig.st_mode)) { enterDir(file->d_name); free(data); }
+        else if (S_ISREG(data->orig.st_mode)) copyFile((void *) data);
+        else free(data);
     }
 
     chdir("..");
